a81af1a30f1a32509908126435b32e54261dfe4a
princethakur19 <princethakur7778@gmail.com>
2025-04-24 11:00:07 +0530

    done with props

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <> 
+    <Navbar title="TextUtils" aboutText="About TextUtils"/>
+    {/* <Navbar/> */}
+    </>
+  );
+}
+
+export default App;
