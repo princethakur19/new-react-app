ace2318b5425d559a63bf71f6e678d40ed760cba
princethakur19 <princethakur7778@gmail.com>
2025-04-25 16:10:59 +0530

    Adding new features using javascript

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,17 +6,32 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLoClick = () =>{
+        console.log("Upper case was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleOnChange = (event) =>{
         setText(event.target.value)
     }
-    const [text, setText] = useState('Enter text here2');
+    const [text, setText] = useState('');
   return (
-    <div>
+    <>
+    <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" value= {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="div container" my-3>
+      <h2>Your text summaer</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+   </>
   )
 }
