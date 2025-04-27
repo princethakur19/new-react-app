e1922ff1b250d78f8d3f7d362e9fac2ad85b905f
princethakur19 <princethakur7778@gmail.com>
2025-04-27 16:37:07 +0530

    Create dark mode in about component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,10 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText)
     }
+    const handleClearClick = () =>{
+        let newText = '';
+        setText(newText)
+    }
     const handleOnChange = (event) =>{
         setText(event.target.value)
     }
@@ -24,6 +28,7 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
     </div>
     <div className="div container" my-3>
       <h2>Your text summaer</h2>
